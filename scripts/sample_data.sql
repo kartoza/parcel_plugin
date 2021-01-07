0a0bf9a0fc2439bb101d0cceb7549d725fb01d06
mazano <kartozanotifications@gmail.com>
2021-01-07 16:19:19 +0200

    Use views instead of mviews and update example (#78)
    * added first working version for QGIS3 - loads properly but need to fix and ensure all works
    
    * renamed files and changed code to be compliant to the new API
    
    * Reformat code so that test can pass
    
    * fix for selecting feature when editng parcels
    
    * fix no database error
    
    * update API call
    
    * Fix setup database and materialised views not working properly
    
    * Changed version number fro QGIS
    
    * Application of styles for layers
    
    * modified sample script
    
    * Modified descriptions and added manual as PDF
    
    * changed version number
    
    * Use views instead of mviews and remove the overhead maintainance of triggers, update sample setup to be self contained
    
    Co-authored-by: myarjunar <myarjunar@gmail.com>

--- scripts/sample_data.sql
+++ scripts/sample_data.sql
@@ -1,12 +1,19 @@
+-- Insert Reference beacon
 INSERT INTO beacons(beacon, y, x, location, name)
 VALUES ('MB1416',735460.877,488470.404,'East','Test');
 
-INSERT INTO beacons (x,y,beacon) VALUES (488444.861,735443.135, 'MB1417');
-INSERT INTO beacons (x,y,beacon) VALUES (488440.719,735456.299, 'MB1418');
-INSERT INTO beacons (x,y,beacon) VALUES (488458.679,735472.068, 'MB1419');
+-- Insert other beacons - This is optional but good for a quick demo
 
+--INSERT INTO beacons (x,y,beacon) VALUES (488444.861,735443.135, 'MB1417');
+--INSERT INTO beacons (x,y,beacon) VALUES (488440.719,735456.299, 'MB1418');
+--INSERT INTO beacons (x,y,beacon) VALUES (488458.679,735472.068, 'MB1419');
+
+-- Populate other tables as needed
 INSERT INTO instrument_cat(description) 
 VALUES('Deed of Assignment'),('Certificate Of Occupancy'),('Deed of Conveyance');
+
+INSERT INTO public.allocation_cat(description, allocation_cat)
+	VALUES ('Allocation Test', 1);
 
 INSERT INTO local_govt( local_govt_name) 
 VALUES ('Test province');
@@ -24,6 +31,11 @@ VALUES ('IF SLR184','BC5 OG','COFO','OGSG','Olawale Olusoga Olubi',38,23,'RIVERV
 
 INSERT INTO survey(plan_no, ref_beacon, scheme)
 VALUES ('BC5 OG', 'MB1416', 1);
+
+-- Insert records into parcel lookup to help with the parcel lookup table
+INSERT INTO parcel_lookup(
+	plot_sn, available, scheme, block, local_govt, prop_type, file_number, allocation, manual_no, deeds_file, official_area, private, status)
+	VALUES ('AS 454', 'true', 1, 'AD243', 1, 2, 'FID123', 1, 'GIS123', 'STA123', 100, 'true',1);
 
 
 
